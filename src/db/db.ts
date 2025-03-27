e88bed3da2f76f46ecc408a8e0c92400b5d55251
Tint Htoo Aung <tinthtooaung0241@gmail.com>
2025-03-27 23:58:43 +0700

    dont know

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,3 +1,9 @@
+// src/db.ts
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
-const db = drizzle(process.env.DATABASE_URL);
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
